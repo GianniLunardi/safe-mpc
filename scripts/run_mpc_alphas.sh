64f4f85a186bd9bbe4565dabd40cd694fcbb5d5e
GianniLunardi <gianni.lunardi96@gmail.com>
2025-03-10 14:22:29 +0100

    update bash files to run all the tests with different controllers and horizons/alphas

--- scripts/run_mpc_alphas.sh
+++ scripts/run_mpc_alphas.sh
@@ -7,30 +7,38 @@ GUESS_SCRIPT="guess.py"
 MPC_SCRIPT="mpc.py"
 
 # Define the output log file
-LOG_PREFIX="$1"
-LOG_GUESS="${LOG_PREFIX}_guess_sm.txt"
-LOG_MPC="${LOG_PREFIX}_mpc_sm.txt"
+LOG_GUESS="guess_sm_hard.txt"
+LOG_MPC="mpc_sm_hard.txt"
 
 # Clear the log file if it exists
 > "$LOG_GUESS"
 > "$LOG_MPC"
 
+# Controller type
+CONT_ARG=("naive" "zerovel" "st" "htwa" "receding" "real")
+
 # Define the arguments for the Python script
-ALPHA_ARG=("20" "30" "40" "50") 
+ALPHA_ARG=("20" "30" "40") # "50") 
 
 # Loop through the arguments
-for ARG in "${ALPHA_ARG[@]}"; do
-    # Guess
-    echo "Running $GUESS_SCRIPT with argument alpha $ARG" | tee -a "$LOG_GUESS"
-    nohup python "$GUESS_SCRIPT" -c=$1 --alpha="$ARG" >> "$LOG_GUESS" 2>&1
-    echo "Completed execution" | tee -a "$LOG_GUESS"
-    echo "----------------------------------------" | tee -a "$LOG_GUESS"
+for CONT in "${CONT_ARG[@]}"; do
+    # Controller
+    echo "Running tasks with controller $CONT" | tee -a "$LOG_GUESS" "$LOG_MPC"
+    echo "-----------------------------------" | tee -a "$LOG_GUESS" "$LOG_MPC"
 
-    # MPC
-    echo "Running $MPC_SCRIPT with argument alpha $ARG" | tee -a "$LOG_MPC"
-    nohup python "$MPC_SCRIPT" -c=$1 --alpha="$ARG" >> "$LOG_MPC" 2>&1
-    echo "Completed execution" | tee -a "$LOG_MPC"
-    echo "----------------------------------------" | tee -a "$LOG_MPC"
+    for ARG in "${ALPHA_ARG[@]}"; do
+        # Guess
+        echo "Running $GUESS_SCRIPT with argument alpha $ARG" | tee -a "$LOG_GUESS"
+        nohup python "$GUESS_SCRIPT" -c="$CONT" --alpha="$ARG" >> "$LOG_GUESS" 2>&1
+        echo "Completed execution" | tee -a "$LOG_GUESS"
+        echo "----------------------------------------" | tee -a "$LOG_GUESS"
+
+        # MPC
+        echo "Running $MPC_SCRIPT with argument alpha $ARG" | tee -a "$LOG_MPC"
+        nohup python "$MPC_SCRIPT" -c="$CONT" --alpha="$ARG" >> "$LOG_MPC" 2>&1
+        echo "Completed execution" | tee -a "$LOG_MPC"
+        echo "----------------------------------------" | tee -a "$LOG_MPC"
+    done
 done
 
 echo "All executions completed. Log written to $LOG_GUESS" and "$LOG_MPC"
